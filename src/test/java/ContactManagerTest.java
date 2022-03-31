ba2650c8d9c369fa4c35c517b470f98e0ffcd64a
Maycon Douglas <maycondss@live.com>
2022-03-30 22:35:26 -0300

    nested Test

--- src/test/java/ContactManagerTest.java
+++ src/test/java/ContactManagerTest.java
@@ -1,3 +1,5 @@
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
@@ -121,61 +123,68 @@ class ContactManagerTest {
 
     }
 
-    @RepeatedTest(value = 5)
-    @DisplayName("Should Create a Contact Repeatedly")
-    public void shouldCreateContactRepeatedly() {
-        //It is a good practice when we are verifying random values.
-        //Instantiate the contact
-        contactManager.addContact("Maycon", "Santos", "5515987654321");
-        // Verify if the list is not empty
-        Assertions.assertFalse(contactManager.getAllContacts().isEmpty());
-        //Verify if there is only one contact on the list.
-        Assertions.assertEquals(1, contactManager.getAllContacts().size());
+
+    @Nested
+    class RepeatedNestedTest{
+        @RepeatedTest(value = 5)
+        @DisplayName("Should Create a Contact Repeatedly")
+        public void shouldCreateContactRepeatedly() {
+            //It is a good practice when we are verifying random values.
+            //Instantiate the contact
+            contactManager.addContact("Maycon", "Santos", "5515987654321");
+            // Verify if the list is not empty
+            Assertions.assertFalse(contactManager.getAllContacts().isEmpty());
+            //Verify if there is only one contact on the list.
+            Assertions.assertEquals(1, contactManager.getAllContacts().size());
+        }
     }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"5512987654321", "5512987654322", "5512987654323"})
-    @DisplayName("Should Create a Contact Repeatedly with parameters for phone number")
-    public void shouldCreateContactParametrized(String phoneNumber) {
-        //It is a good practice when we are verifying random values.
-        //Instantiate the contact
-        contactManager.addContact("Maycon", "Santos", phoneNumber);
-        // Verify if the list is not empty
-        Assertions.assertFalse(contactManager.getAllContacts().isEmpty());
-        //Verify if there is only one contact on the list.
-        Assertions.assertEquals(1, contactManager.getAllContacts().size());
+    @Nested
+    class ParameterizedNestedTest{
+
+        private List<String> phoneNumberList() {
+            return Arrays.asList("5512987654321", "5512987654322", "5512987654323");
+        }
+        @ParameterizedTest
+        @ValueSource(strings = {"5512987654321", "5512987654322", "5512987654323"})
+        @DisplayName("Should Create a Contact Repeatedly with parameters for phone number")
+
+        public void shouldCreateContactParametrized(String phoneNumber) {
+            //It is a good practice when we are verifying random values.
+            //Instantiate the contact
+            contactManager.addContact("Maycon", "Santos", phoneNumber);
+            // Verify if the list is not empty
+            Assertions.assertFalse(contactManager.getAllContacts().isEmpty());
+            //Verify if there is only one contact on the list.
+            Assertions.assertEquals(1, contactManager.getAllContacts().size());
+        }
+
+        @ParameterizedTest
+        @Disabled
+        @MethodSource("phoneNumberList")
+        @DisplayName("Should Create a Contact Repeatedly with parameters with Method Source for phone number")
+        public void shouldCreateContactParametrizedWithMethodSource(String phoneNumber) {
+            //It is a good practice when we are verifying random values.
+            //Instantiate the contact
+            contactManager.addContact("Maycon", "Santos", phoneNumber);
+            // Verify if the list is not empty
+            Assertions.assertFalse(contactManager.getAllContacts().isEmpty());
+            //Verify if there is only one contact on the list.
+            Assertions.assertEquals(1, contactManager.getAllContacts().size());
+        }
+
+        @ParameterizedTest
+        @CsvFileSource(resources = "/data.csv")
+        @DisplayName("Should Create a Contact Repeatedly with parameters for phone number from CSV")
+        public void shouldCreateContactParametrizedWithPhoneNumberFromCSVSource(String phoneNumber) {
+            //It is a good practice when we are verifying random values.
+            //Instantiate the contact
+            contactManager.addContact("Maycon", "Santos", phoneNumber);
+            // Verify if the list is not empty
+            Assertions.assertFalse(contactManager.getAllContacts().isEmpty());
+            //Verify if there is only one contact on the list.
+            Assertions.assertEquals(1, contactManager.getAllContacts().size());
+        }
+
     }
-
-
-    @ParameterizedTest
-    @MethodSource("phoneNumberList")
-    @DisplayName("Should Create a Contact Repeatedly with parameters with Method Source for phone number")
-    public void shouldCreateContactParametrizedWithMethodSource(String phoneNumber) {
-        //It is a good practice when we are verifying random values.
-        //Instantiate the contact
-        contactManager.addContact("Maycon", "Santos", phoneNumber);
-        // Verify if the list is not empty
-        Assertions.assertFalse(contactManager.getAllContacts().isEmpty());
-        //Verify if there is only one contact on the list.
-        Assertions.assertEquals(1, contactManager.getAllContacts().size());
-    }
-
-    private static List<String> phoneNumberList() {
-        return Arrays.asList("5512987654321", "5512987654322", "5512987654323");
-    }
-
-    @ParameterizedTest
-    @CsvFileSource(resources = "/data.csv")
-    @DisplayName("Should Create a Contact Repeatedly with parameters for phone number from CSV")
-    public void shouldCreateContactParametrizedWithPhoneNumberFromCSVSource(String phoneNumber) {
-        //It is a good practice when we are verifying random values.
-        //Instantiate the contact
-        contactManager.addContact("Maycon", "Santos", phoneNumber);
-        // Verify if the list is not empty
-        Assertions.assertFalse(contactManager.getAllContacts().isEmpty());
-        //Verify if there is only one contact on the list.
-        Assertions.assertEquals(1, contactManager.getAllContacts().size());
-    }
-
-
 }
